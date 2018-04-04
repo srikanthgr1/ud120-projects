f7ee768b7287e11fc2e8bc04dbca757ebd24d79c
Srikanth GR <srikanthgr1@gmail.com>
2018-04-05 02:02:20 +0530

    convert .pkl files to unix format
    The file gets the characters garbles while opened and uploaded from windows
    so run this program to fix issues in .pkl files

---
+++ tools/dos2unix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+convert dos linefeeds (crlf) to unix (lf)
+usage: dos2unix.py 
+"""
+original = "email_authors.pkl"
+destination = "email_authors_ux.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+infile.close()
+output.close()
+print("Done. Saved %s bytes.",  (len(content)-outsize))
